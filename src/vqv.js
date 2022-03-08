e4f0479103727cf5afb1d81942f8ab6281f28fde
Gisele <gisele.viedenhelfen@gmail.com>
2022-03-08 18:07:36 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
-
+const vqv = (n, i) => {
+if (typeof n !== 'string' || typeof i !== 'number') {
+  return undefined;
+}
+return `Oi, meu nome é ${n}!
+Tenho ${i} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+console.log(vqv());
 module.exports = vqv;
